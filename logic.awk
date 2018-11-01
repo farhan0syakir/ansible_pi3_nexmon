d8d95f26b371832918e0d21b658f1c7a178f3d87
farhan <farhan@alumni.ui.ac.id>
2018-11-01 16:36:49 +0700

    push verify

--- logic.awk
+++ logic.awk
@@ -1,6 +1,6 @@
 BEGIN {   FS = ","}
 {
-    if (curr_time < $2)   # oops! should be a == b
+    if (one_minute_ago < $2)   # oops! should be a == b
         #print curr_time" sebelum "$1 # ini yang kita ambil
         print $1","$2","$3","$4
     #else
--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-curr_time="2018-10-31 17:40:55"
-d1=`date -d "1 minute ago" +%s`
+# one_minute_ago=`date -d "1 minute ago" +%s`
+one_minute_ago="2018-10-31 17:40:55"
 # d1=`date -d "1 minute ago"`
-# echo "$d1"
 
 awk 'f;/Station/{f=1}' coba-02.csv |awk '{ print $1$2" "$3$4" "$5$6 }'|sort --unique | grep -v "DA:A1:19" > result.csv
 
 #run awk
-awk -vcurr_time="$curr_time" -f logic.awk result.csv 
+awk -vone_minute_ago="$one_minute_ago" -f logic.awk result.csv > res_2.csv
+#versify
+awk -F"," '{print $3}' res_2.csv | sort
